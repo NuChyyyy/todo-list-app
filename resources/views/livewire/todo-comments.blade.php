545f4969495a06e993cbde8ac44802f4c139903e
Keng <nuchyyyy113112@gmail.com>
2025-06-11 01:14:13 +0700

    Edit comment component: add show/hide comment icon and comment scroll bar

--- resources/views/livewire/todo-comments.blade.php
+++ resources/views/livewire/todo-comments.blade.php
@@ -1,31 +1,41 @@
 <div class="mt-2 pt-1">
-    <flux:button variant="subtle" wire:click="toggleComments" :loading="false" size="xs">
-        {{ $showComments ? 'Hide Comments' : 'Show Comments' }}
-    </flux:button>
+        @if ($showComments)
+            <flux:button variant="subtle" wire:click="toggleComments" :loading="false" size="xs" icon:trailing="arrow-up">
+                Hide Comments
+            </flux:button>
+        @else
+            <flux:button variant="subtle" wire:click="toggleComments" :loading="false" size="xs" icon:trailing="arrow-down">
+                Show Comments
+            </flux:button>
+        @endif
     <div class="mt-2">
         @if($showComments)
             <flux:textarea wire:model="commentText" rows="auto" placeholder="Write a comment..." />
             <div class="flex justify-between mt-2">
-                <flux:button wire:click="addComment" class="mr-2" :loading="false">comment</flux:button>
-                <flux:input type="file" wire:model="image" />
+                <flux:button wire:click="addComment" size="sm" :loading="false">Comment</flux:button>
+                <flux:input type="file" size="sm" wire:model="image" class="mx-2" />
             </div>
-            <div class="mt-4 space-y-3 max-h-48 overflow-y-auto pr-2">
+            <div class="mt-4 space-y-3 max-h-72 overflow-y-scroll pr-2">
                 @foreach($comments as $comment)
-                    <div class="p-3 rounded-xl">
-                        <div class="flex justify-between items-center mb-1">
-                            <flux:text class="text-m mt-1" variant="strong"> {{ $todo->user->name ?? 'ไม่ทราบ' }}</flux:text>
+                    <div class="rounded-xl">
+                        <div class="flex justify-between mb-1 items-start">
+                            <div class="flex flex-row items-start">
+                                <flux:profile circle :chevron="false" />
+                                <div>
+                                    <flux:text class="text-m mx-1" variant="strong"> {{ $comment->user->name ?? 'ไม่ทราบชื่อ' }}</flux:text>
+                                    <flux:text class="mx-1">{{ $comment->body }}</flux:text>
+                                    @if($comment->image_path)
+                                        <img src="{{ Storage::url($comment->image_path) }}"
+                                            class="mt-2 max-w-64 object-cover rounded-lg shadow" />
+                                    @endif
+                                </div>
+                            </div>
                             @if($comment->user_id === auth()->id())
                                 <button wire:click="deleteComment({{ $comment->id }})">
-                                    <flux:icon.trash class="w-4 h-4 text-red-500" />
+                                    <flux:icon.trash class="w-4 h-4 text-red-500 mt-1" title="delete your comment"/>
                                 </button>
                             @endif
                         </div>
-                        <flux:text class="mt-2">{{ $comment->body }}</flux:text>
-
-                        @if($comment->image_path)
-                            <img src="{{ Storage::url($comment->image_path) }}"
-                                class="mt-2 w-32 h-32 object-cover rounded-lg shadow" />
-                        @endif
                     </div>
                 @endforeach
             </div>
