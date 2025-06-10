839e6ced23956dc32c69ab02a58c12f358d9f5de
Keng <nuchyyyy113112@gmail.com>
2025-06-11 01:24:23 +0700

    Change logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="flex items-center">
-    <flux:icon.calendar />
+    <flux:checkbox checked :loading="false" />
     <span class="truncate leading-tight font-semibold text-lg mx-2">Todo list</span>
 </div>
