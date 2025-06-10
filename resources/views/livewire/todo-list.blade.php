6b96df9968113a761e3f1cd3152f7a9442073968
Keng <nuchyyyy113112@gmail.com>
2025-06-11 01:16:11 +0700

    Update  UI of todo-list component (use Flux UI)

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,13 +1,17 @@
-<div class="max-w-lg mx-auto mt-10">
-    <form wire:submit.prevent="addTodo" class="flex gap-2 mb-4">
-        <input type="text" wire:model="newTodo" class="border rounded px-3 py-2 w-full" placeholder="Add a task...">
-        <button class="bg-rose-400 text-black px-4 py-2 rounded">Add</button>
-    </form>
+@php use Illuminate\Support\Str; @endphp
 
+<div class="max-w-lg mx-auto mt-10">
+    <flux:field class="mb-4">
+        <div class="flex justify-between gap-2">
+            <flux:input placeholder="Add a task..." wire:model="newTodo"/>
+            <flux:button variant="primary" wire:click="addTodo" :loading="false">Add</flux:button>
+        </div>
+    </flux:field>
     <ul class="space-y-2">
         @if($todos->count())
             @foreach($todos as $todo)
-                <li wire:key="todo-{{ $todo->id }}">
+                <li wire:key="todo-{{ $todo->id }}"
+                    class="p-3 rounded-lg {{ $todo->is_done ? '' : ' bg-zinc-800' }}">
                     @if($editingId === $todo->id)
                         <div class="space-y-2">
                             <flux:textarea placeholder="Filter by..." wire:model.defer="editingTitle" />
@@ -18,28 +22,33 @@
                         </div>
                     @endif
                     <div class="flex justify-between items-center">
+                        @if (!$editingId)
                         <div class="flex items-center gap-2">
-                            <input type="checkbox" wire:click="toggleDone({{ $todo->id }})" {{ $todo->is_done ? 'checked' :'' }}>
-                            <span class="{{ $todo->is_done ? 'line-through text-zinc-400 text-xs' : 'text-lg' }}">{{ $todo->title }}</span>
-                           
+                            <input type="checkbox" wire:click="toggleDone({{ $todo->id }})" {{ $todo->is_done ? 'checked' : '' }}>
+                            <span title="{{ $todo->title }}" class="{{ $todo->is_done ? 'line-through text-zinc-400 text-md' : 'text-lg' }}">
+                                {{ Str::limit($todo->title, 20) }}
+                            </span>
                         </div>
                         <div class="flex items-center gap-2">
                             @if($todo->user_id === auth()->id())
                                 @if (!$todo->is_done)
-                                <button wire:click="startEdit({{ $todo->id }})" title="แก้ไข">
-                                    <flux:icon.pencil class="w-5 h-5" />
-                                </button>
-                                @endif
-                                <button wire:click="deleteTodo({{ $todo->id }})" title="ลบ">
+                                    <button wire:click="startEdit({{ $todo->id }})" title="edit your task">
+                                        <flux:icon.pencil class="w-5 h-5" />
+                                    </button>
+                                    @endif
+                                <button wire:click="deleteTodo({{ $todo->id }})" title="delete your task">
                                     <flux:icon.trash class="w-5 h-5 text-red-500" />
                                 </button>
                             @endif
                         </div>
+                        @endif
                     </div>
                     <div class="flex justify-between mt-1">
                         <flux:text class="text-xs">Create by {{ $todo->user->name ?? 'ไม่ทราบ' }}</flux:text>
                         @if($todo->is_done)
-                            <flux:text class="text-xs"> Checked by {{ $todo->checker->name ?? 'Unknown' }} at {{ $todo->updated_at->format('d M Y H:i') }} </flux:text>
+                            <flux:text class="text-xs">Checked by {{ $todo->checker->name ?? 'Unknown' }} at
+                                {{ $todo->updated_at->format('d M Y H:i') }}
+                            </flux:text>
                         @endif
                     </div>
                     <livewire:todo-comments :todo="$todo->toArray()" :key="'todo-comments-' . $todo->id" />
